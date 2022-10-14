46436f1b0fdd446929e3e3a3d41a08af93ce3105
Franco Lanzillotta <flanzi@fi.mdp.edu.ar>
2022-10-14 11:03:28 -0300

    mutex replaced by 1-size semaphore

--- main.c
+++ main.c
@@ -9,7 +9,7 @@
 
 sem_t empty;
 sem_t full;
-pthread_mutex_t mutex;
+sem_t mutex;
 
 int produce() {
   sleep(1);
@@ -27,9 +27,9 @@ void *producer(void * arg) {
   for (int i = 0; i < MAX; i++) {
     int value = produce();
     sem_wait(&empty);
-    pthread_mutex_lock(&mutex);
+    sem_wait(&mutex);
     buffer_insert(buffer, value);
-    pthread_mutex_unlock(&mutex);
+    sem_post(&mutex);
     sem_post(&full);
   }
   pthread_exit(NULL);
@@ -39,9 +39,9 @@ void *consumer(void * arg) {
   buffer_t *buffer = (buffer_t *) arg;
   for (int i = 0; i < MAX; i++) {
     sem_wait(&full);
-    pthread_mutex_lock(&mutex);
+    sem_wait(&mutex);
     int value = buffer_remove(buffer);
-    pthread_mutex_unlock(&mutex);
+    sem_post(&mutex);
     sem_post(&empty);
     consume(value);
   }
@@ -53,14 +53,14 @@ int main() {
   buffer_t buffer;
   srand(time(NULL));
   buffer_init(&buffer);
-  pthread_mutex_init(&mutex, NULL);
   sem_init(&empty, 0, BUFF_SIZE);
   sem_init(&full, 0, 0);
+  sem_init(&mutex, 0, 1);
   pthread_create(&pro, NULL, &producer, (void *) &buffer);
   pthread_create(&con, NULL, &consumer, (void *) &buffer);
   pthread_join(pro, NULL);
   pthread_join(con, NULL);
   sem_destroy(&full);
   sem_destroy(&empty);
-  pthread_mutex_destroy(&mutex);
+  sem_destroy(&mutex);
 }
